869f1b64d3d24a92e924f600e4c28967b17a6c1c
acenorth <north.andy@gmail.com>
2016-11-21 16:55:44 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,91 @@
+var tessel = require('tessel');
+var rfidlib = require('rfid-pn532');
+var servoLib = require('servo-pca9685');
+
+var rfid = rfidlib.use(tessel.port['A']); 
+var servo = servoLib.use(tessel.port['B']);
+
+var blinky;
+var min = 1, max = 5;
+var slapNumber = randInRange(min, max, true);
+var port = 1;
+
+function randInRange(min, max, inclusive) {
+	return Math.floor(Math.random() * (max - min + (+!!inclusive))) + min;
+}
+
+function slapThem() {
+	// Do the slapping
+
+	servo.read(port, function(err, reading) {
+		console.log("READING: ",reading);
+
+		servo.move(port, 0.5, function() {
+			console.log('I slapped them');
+			setTimeout(function() {
+				servo.move(port, 0.05);
+			}, 1000);
+		});
+	});
+}
+
+var rfidRdy = new Promise(function(resolve, reject) {
+	rfid.on('ready', function(card) {
+		resolve(card);
+	});
+});
+
+var servoRdy = new Promise(function(resolve, reject) {
+	servo.on('ready', function() {
+		resolve();
+	});
+});
+
+Promise.all([rfidRdy, servoRdy])
+	.then(function(arr) {
+
+		return new Promise(function(resolve, reject) {
+			servo.configure(port, 0.05, 0.5, function() {
+				resolve(arr);
+			});
+		});
+	})
+	.then(function(arr) {
+		var card = arr[0];
+		console.log(slapNumber);
+		tessel.led[2].on();
+
+		rfid.on('read', function(card) {
+			if (!blinky){
+
+				blinky = setInterval(function () {
+					tessel.led[2].toggle();
+					tessel.led[3].toggle();
+				}, 200);
+
+				setTimeout(function(){
+					clearInterval(blinky);
+					blinky = undefined;
+					var rand = randInRange(min, max, true);
+					console.log("RANDO NUM: ",rand);
+					if (rand === slapNumber) {
+						slapThem();
+					}
+				}, 2000);
+			}
+
+		});
+	})
+	.catch(function(e) {
+		console.log(e);
+	});
+
+rfid.on('ready', function (version) {
+  console.log('Ready to read RFID card');
+
+
+});
+
+rfid.on('error', function (err) {
+  console.error(err);
+});
